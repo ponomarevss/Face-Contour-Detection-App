64e80a1ec40af7fc444bf6081e000898100809d1
dev.ponomarevss <dev.ponomarevss@gmail.com>
2025-03-31 18:36:42 +0500

    Initial commit. listAvailableCameras(), listCameraCharacteristics()

---
+++ app/src/main/java/ru/sspo/facecontourdetectionapp/CameraViewFragment.kt
@@ -0,0 +1,94 @@
+package ru.sspo.facecontourdetectionapp
+
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.sspo.facecontourdetectionapp.databinding.FragmentCameraViewBinding
+
+class CameraViewFragment : Fragment() {
+
+    private var _binding: FragmentCameraViewBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var cameraManager: CameraManager
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCameraViewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Получаем экземпляр CameraManager
+        cameraManager = requireContext().getSystemService(CameraManager::class.java)
+
+        // Перечисляем доступные камеры
+//        listAvailableCameras()
+
+        // Перечисляем доступные камеры и получаем их характеристики
+        listCameraCharacteristics()
+    }
+
+    private fun listAvailableCameras() {
+        try {
+            // Получаем список идентификаторов камер
+            val cameraIdList = cameraManager.cameraIdList
+            for (cameraId in cameraIdList) {
+                // Получаем характеристики камеры
+                val characteristics: CameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId)
+                val cameraFacing = characteristics.get(CameraCharacteristics.LENS_FACING)
+
+                // Проверяем, является ли камера фронтальной или задней
+                if (cameraFacing == CameraCharacteristics.LENS_FACING_FRONT) {
+                    Log.d("CameraActivity", "Доступна фронтальная камера: $cameraId")
+                } else if (cameraFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    Log.d("CameraActivity", "Доступна задняя камера: $cameraId")
+                }
+            }
+        } catch (e: CameraAccessException) {
+            Log.e("CameraActivity", "Ошибка доступа к камере: ${e.message}")
+        }
+    }
+
+    private fun listCameraCharacteristics() {
+        try {
+            // Получаем список идентификаторов камер
+            val cameraIdList = cameraManager.cameraIdList
+            for (cameraId in cameraIdList) {
+                // Получаем характеристики камеры
+                val characteristics: CameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId)
+
+                // Пример получения информации о характеристиках
+                val lensFacing = characteristics.get(CameraCharacteristics.LENS_FACING)
+                val sensorOrientation = characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION)
+
+                // Логируем информацию о камере
+                Log.d("CameraFragment", "Камера ID: $cameraId")
+                Log.d("CameraFragment", "Направление объектива: $lensFacing")
+                Log.d("CameraFragment", "Ориентация сенсора: $sensorOrientation")
+            }
+        } catch (e: CameraAccessException) {
+            Log.e("CameraFragment", "Ошибка доступа к камере: ${e.message}")
+        }
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/ru/sspo/facecontourdetectionapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package ru.sspo.facecontourdetectionapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.sspo.facecontourdetectionapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            val cameraViewFragment = CameraViewFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(binding.fragmentContainer.id, cameraViewFragment)
+                .commit()
+        }
+    }
+}
