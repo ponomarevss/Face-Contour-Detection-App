8f7db3792205863e483c0416d0bf04c466094d8a
dev.ponomarevss <dev.ponomarevss@gmail.com>
2025-04-02 14:19:24 +0500

    permission required and checked

--- app/src/main/java/ru/sspo/facecontourdetectionapp/CameraViewFragment.kt
+++ app/src/main/java/ru/sspo/facecontourdetectionapp/CameraViewFragment.kt
@@ -1,13 +1,29 @@
 package ru.sspo.facecontourdetectionapp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
+import android.hardware.camera2.params.SessionConfiguration
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.view.SurfaceView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import ru.sspo.facecontourdetectionapp.databinding.FragmentCameraViewBinding
 
@@ -17,6 +33,29 @@ class CameraViewFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var cameraManager: CameraManager
+    private lateinit var requestCameraPermissionLauncher: ActivityResultLauncher<String>
+
+    private lateinit var surfaceView: SurfaceView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        permissionRequest()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun permissionRequest() {
+        // Инициализация ActivityResultLauncher для запроса разрешения на камеру
+        requestCameraPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                openCamera()
+            } else {
+                Toast.makeText(requireContext(), "Требуется разрешение", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,24 +67,58 @@ class CameraViewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding?.let { surfaceView = it.surfaceView }
 
         // Получаем экземпляр CameraManager
         cameraManager = requireContext().getSystemService(CameraManager::class.java)
 
-        // Перечисляем доступные камеры
-//        listAvailableCameras()
-
-        // Перечисляем доступные камеры и получаем их характеристики
-        listCameraCharacteristics()
+        // Проверяем разрешение на камеру
+        checkCameraPermission()
     }
 
+    private fun checkCameraPermission() {
+        val permission =
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            openCamera()
+        } else {
+            requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+
+    @RequiresPermission(Manifest.permission.CAMERA)
+    private fun openCamera() {
+        val cameraId = cameraManager.cameraIdList[0]
+        try {
+            cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
+                override fun onOpened(p0: CameraDevice) {
+
+                }
+
+                override fun onDisconnected(p0: CameraDevice) {
+                    p0.close()
+                }
+
+                override fun onError(p0: CameraDevice, p1: Int) {
+                    p0.close()
+                    Log.e("CameraFragment", "Ошибка открытия камеры: $p1")
+                }
+            }, null)
+        } catch (e: CameraAccessException) {
+            Log.e("CameraFragment", "Ошибка доступа к камере: ${e.message}")
+        }
+    }
+
+
+    // Перечисляем доступные камеры
     private fun listAvailableCameras() {
         try {
             // Получаем список идентификаторов камер
             val cameraIdList = cameraManager.cameraIdList
             for (cameraId in cameraIdList) {
                 // Получаем характеристики камеры
-                val characteristics: CameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId)
+                val characteristics: CameraCharacteristics =
+                    cameraManager.getCameraCharacteristics(cameraId)
                 val cameraFacing = characteristics.get(CameraCharacteristics.LENS_FACING)
 
                 // Проверяем, является ли камера фронтальной или задней
@@ -60,17 +133,20 @@ class CameraViewFragment : Fragment() {
         }
     }
 
+    // Перечисляем доступные камеры и их характеристик
     private fun listCameraCharacteristics() {
         try {
             // Получаем список идентификаторов камер
             val cameraIdList = cameraManager.cameraIdList
             for (cameraId in cameraIdList) {
                 // Получаем характеристики камеры
-                val characteristics: CameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId)
+                val characteristics: CameraCharacteristics =
+                    cameraManager.getCameraCharacteristics(cameraId)
 
                 // Пример получения информации о характеристиках
                 val lensFacing = characteristics.get(CameraCharacteristics.LENS_FACING)
-                val sensorOrientation = characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION)
+                val sensorOrientation =
+                    characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION)
 
                 // Логируем информацию о камере
                 Log.d("CameraFragment", "Камера ID: $cameraId")
